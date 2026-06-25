77df3405322575628fb62eb1583fc4d0c557d1e2
anim710 <anim3654@gmail.com>
2026-06-26 01:01:00 +0600

    added page file in main

---
+++ src/app/(main)/page.jsx
@@ -0,0 +1,17 @@
+import HeroBanner from "@/components/home/HeroBanner";
+import AdvertisedTickets from "@/components/home/AdvertisedTickets";
+import LatestTickets from "@/components/home/LatestTickets";
+import WhyChooseUs from "@/components/home/WhyChooseUs";
+import PopularRoutes from "@/components/home/PopularRoutes";
+
+export default function HomePage() {
+  return (
+    <div>
+      <HeroBanner />
+      <AdvertisedTickets />
+      <LatestTickets />
+      <WhyChooseUs />
+      <PopularRoutes />
+    </div>
+  );
+}
