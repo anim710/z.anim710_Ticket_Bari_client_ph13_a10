e39c26196dc1cda652edd3a090351dbc772a0062
anim710 <anim3654@gmail.com>
2026-06-26 00:17:39 +0600

    added axios file

---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">TicketBari ✅</h1>
+    </div>
+  );
+}
